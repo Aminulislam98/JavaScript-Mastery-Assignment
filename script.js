89bd3cb145ecdf0e267a028430778bf5e6b2e2d4
Aminul islam <aminurrohman98@gmail.com>
2026-06-28 21:14:12 +0100

    Assignment: Data Types  ✅

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 "use strict";
 // Assignment: Values and Variables
-// let country = "Bangladesh";
-// let continent = "Asia";
-// let population = "16 Million";
-// console.log("Country Name:", country);
-// console.log("Continent:", continent);
+let country = "Bangladesh";
+let continent = "Asia";
+let population = "16 Million";
+console.log("Country Name:", country);
+console.log("Continent:", continent);
 // console.log("Population:", population);
+
+// Assignment: Data Types
+const isIsland = false;
+let language;
+
+console.log(
+  typeof country,
+  typeof continent,
+  typeof population,
+  typeof isIsland,
+  typeof language,
+);
