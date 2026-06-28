602c6b3dc85454d2537d3dd852238c6da68fe60a
Aminul islam <aminurrohman98@gmail.com>
2026-06-28 21:11:01 +0100

    Assignment: Values and Variables ✅

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 "use strict";
+// Assignment: Values and Variables
+// let country = "Bangladesh";
+// let continent = "Asia";
+// let population = "16 Million";
+// console.log("Country Name:", country);
+// console.log("Continent:", continent);
+// console.log("Population:", population);
