e8d26c7610433a75934002ce900d9d9537f1f2bb
Aminul islam <aminurrohman98@gmail.com>
2026-06-29 13:22:50 +0100

    Assignment: Iteration: The for Loop  ✅

--- script.js
+++ script.js
@@ -191,3 +191,8 @@ const as = myCountry.describe();
 console.log(as);
 myCountry.checkIsland();
 console.log(myCountry);
+
+// Assignment: Iteration: The for Loop
+for (let i = 1; i <= 50; i++) {
+  console.log(`Voter number ${i} is currently voting....`);
+}
